09444bf43585fa05ab8ed0e36ce44e1d9396f0f7
majkojordan <m.snincak@gmail.com>
2019-11-25 13:45:43 +0100

    data transformer group interactions and calculate weight

--- scripts/data_analyser.py
+++ scripts/data_analyser.py
@@ -2,20 +2,38 @@ import numpy as np
 import pandas as pd
 
 IN_FILE = 'data/dataset.csv'
-FIELDS_TO_ANALYZE = pd.read_csv(IN_FILE, nrows=1).columns.tolist()
+ANALYZE_BY_FIELDS = False
 
 try:
   pd.set_option('display.max_columns', None)
-  for field in FIELDS_TO_ANALYZE:
-    data = pd.read_csv(IN_FILE, usecols=[field])
-    
-    if field == 'timestamp':
-      print('timestamp_analysis: ')
+
+  if ANALYZE_BY_FIELDS:
+    fields = pd.read_csv(IN_FILE, nrows=1).columns.tolist()
+    print(fields)
+
+    for field in fields:
+      data = pd.read_csv(IN_FILE, usecols=[field])
+      
+      if field == 'timestamp':
+        print('timestamp analysis: ')
+        print(min(data['timestamp']))
+        print(max(data['timestamp']))
+
+      print(data.isnull().sum())
+      print(data.describe(include='all'))
+      print(data.dtypes)
+
+  else:
+      data = pd.read_csv(IN_FILE)
+      print('timestamp analysis: ')
       print(min(data['timestamp']))
       print(max(data['timestamp']))
 
-    print(data.isnull().sum())
-    print(data.describe(include='all'))
-    print(data.dtypes)
+      print(data.isnull().sum())
+      print(data.describe(include='all'))
+      print(data.dtypes)
+
+      print('average user interactions per item: ')
+      print(data.groupby(['title'])['customer_id'].count().mean())
 except:
   print('error - check input parameters')
--- scripts/data_transformer.py
+++ scripts/data_transformer.py
@@ -1,13 +1,19 @@
 import numpy as np
 import pandas as pd
+import math
 
-IN_FILE = 'data/recent/recent_merged.csv'
+IN_FILE = 'data/recent/recent_merged_hour.csv'
 OUT_FILE = 'data/dataset.csv'
-PURCHASE_WEIGHT = 5
+PURCHASE_WEIGHT = 3
+MIN_INTERACTIONS = 5
 
 try:
   data = pd.read_csv(IN_FILE)
-  data['event_type'] = data['event_type'].map({'view_item': 1, 'purchase_item': PURCHASE_WEIGHT})
+  data['weight'] = data['event_type'].map({'view_item': 1, 'purchase_item': PURCHASE_WEIGHT})
+
+  data = data.groupby(['customer_id', 'title']).agg({'customer_id': 'first', 'title': 'first', 'weight': sum})
+
+  data['weight'] = data['weight'].apply(lambda x: 1 + math.log(x))
   data.to_csv(OUT_FILE, index=False)
 except:
   print('error - check input parameters')
